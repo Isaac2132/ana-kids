ba5d2244404a554b63e6ae420d30db1b8462a33d
Isaac Barbosa <isaacbarbosadutra768@gmail.com>
2026-08-11 22:17:15 -0300

    atualizado

--- app/data/products.ts
+++ app/data/products.ts
@@ -109,7 +109,7 @@ export const products: Product[] = [
     name: "Vestido Vermelho",
     description: "7 até 9 anos",
     price: 80.0,
-    image: "/produtos/vestido-vermelho.jpeg",
+    image: "/produtos/Vestido-vermelho.jpeg",
     //category: "Vestidos",
   },
 ];
